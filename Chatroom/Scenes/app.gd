39bb5de92786195ed25a3beac6f0914b3117bb0d
Ian Floyd <ianfloydtechnology@gmail.com>
2022-12-19 13:02:54 -0800

    added comments

--- Chatroom/Scenes/app.gd
+++ Chatroom/Scenes/app.gd
@@ -32,12 +32,12 @@ func _process(delta):
 		else:
 			message.text = ""
 
-
+#when host button is pressed, create a server
 func _on_host_pressed():
 	print("host button pressed")
 	create_server()
 
-
+#when host button is pressed, join a server
 func _on_join_pressed():
 	join_server()
 	print("join button pressed")
@@ -45,35 +45,46 @@ func _on_join_pressed():
 
 
 func create_server():
-	#changed from godot 3.x, used to be NetworkedMultiplayerEnet
+	#hide buttons
 	join_button.hide()
 	host_button.hide()
+	
+	#changed from godot 3.x, used to be NetworkedMultiplayerEnet
 	var server : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 	var gateway := SceneMultiplayer.new()
 	
+	#create server on specified port
 	server.create_server(9999, 32)
 	get_tree().set_multiplayer(gateway, self.get_path())
 	multiplayer.set_multiplayer_peer(server)
-	status.text = "Status: Hosting Server"
-	print(get_tree().get_multiplayer().get_unique_id())
 	
+	#update status
+	status.text = "Status: Hosting Server"
+	#unique network id for server is 0
+	print(get_tree().get_multiplayer().get_unique_id())
+
+#called when join button is pressed	
 func join_server():
+	#hide buttons
 	join_button.hide()
 	host_button.hide()
 	
+	#create client on the multiplayer network
 	var client : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
-	var gateway := SceneMultiplayer.new()
-	
+	var gateway := SceneMultiplayer.new()	
 	client.create_client(ip_address.text,9999)
 	get_tree().set_multiplayer(gateway, self.get_path())
 	multiplayer.set_multiplayer_peer(client)
+	
+	#update status
 	status.text = "Status: Joined server"
 
+#updates chat history on all clients
 @rpc(any_peer)	
 func send_chat(txt):
 	history.text += txt
 	history.text += ""
 
-#We're using richtextlabel which allows us to format
+#create message using richtextlabel which allows us to format color
 func create_message() -> String:
 	return "[b][color=" + colors.text +"]" + user_name.text + ": "+"[/color][/b]" + message.text + "\n"
